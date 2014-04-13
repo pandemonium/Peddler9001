c674181d14badf2ac44132bc2548d2ba879bd54b
Patrik Andersson <pandersson@gmail.com>
2014-04-13 15:34:53 +0200

    Little refactorings of Parcel and some cleaning up of the handling of UP#Transaction:s.

--- src/main/scala/subscription/application.scala
+++ src/main/scala/subscription/application.scala
@@ -1,14 +1,17 @@
 package paermar
 package application
 
-import paermar.model.Domain
 import paermar.model.Domain.UnifiedPersistence
-import java.util.Date
 import scala.slick.jdbc.JdbcBackend
 import org.joda.time.DateTime
 
 object Parcel {
   type ⊕ [A, B] = Parcel[A, B]
+
+  implicit class ParcelOps[A](value: A) {
+    def successfulParcel[X, AA <: A] = Parcel successful value
+    def failedParcel     = Parcel failed value
+  }
 
   sealed trait Parcel[+X, +A] { self ⇒
     def isSuccessful: Boolean
@@ -49,17 +52,6 @@ object Parcel {
   }
 }
 
-object Reporting {
-  sealed trait Level
-  case object Info extends Level
-  case object Warning extends Level
-  case object Error extends Level
-
-  sealed trait Report {
-
-  }
-}
-
 trait FeatureUniverse {
   import Parcel._
   type UP = UnifiedPersistence
@@ -67,7 +59,7 @@ trait FeatureUniverse {
   val persistence: UnifiedPersistence
   val database:    JdbcBackend#Database
 
-  def parcelMap[X, A](f: JdbcBackend#Session ⇒ X ⊕ A): X ⊕ A = database withSession f
+  def databaseMap[X, A](f: JdbcBackend#Session ⇒ X ⊕ A): X ⊕ A = database withSession f
 }
 
 trait AuthenticationFeatures { self: FeatureUniverse ⇒
@@ -76,7 +68,7 @@ trait AuthenticationFeatures { self: FeatureUniverse ⇒
 
   case class AuthenticationContext(fullName: String)
 
-  def authenticate(login: String, password: String): String ⊕ AuthenticationContext = parcelMap { implicit session ⇒
+  def authenticate(login: String, password: String): String ⊕ AuthenticationContext = databaseMap { implicit session ⇒
       val authenticAccount = for (name <- persistence.findAuthenticatedAccount(login, password).firstOption)
         yield successful(AuthenticationContext(name))
 
@@ -88,15 +80,15 @@ trait CustomerFeatures { self: FeatureUniverse ⇒
   import persistence.profile.simple._
   import Parcel._
 
-  def customers: String ⊕ Seq[UP#Customer] = parcelMap { implicit session ⇒
-    successful(persistence.customers.buildColl)
+  def customers: String ⊕ Seq[UP#Customer] = databaseMap { implicit session ⇒
+    persistence.customers.buildColl.successfulParcel
   }
 
-  def addCustomer(name: String): String ⊕ UP#Customer = parcelMap { implicit session ⇒
+  def addCustomer(name: String): String ⊕ UP#Customer = databaseMap { implicit session ⇒
     val id       = persistence insertCustomer name
     val customer = persistence findCustomerById id firstOption
 
-    customer map successful getOrElse failed("customer.created_yet_not_found")
+    customer map successful getOrElse "customer.created_yet_not_found".failedParcel
   }
 }
 
@@ -105,31 +97,39 @@ trait TransactionFeatures { self: FeatureUniverse with CustomerFeatures ⇒
   import persistence.TransactionType._
   import Parcel._
 
-  def transactions(from: DateTime, through: DateTime): String ⊕ Seq[UP#Transaction] = parcelMap { implicit session ⇒
+  class ValidatingTransactionExtractor(val `type`: TransactionType) {
+    def unapply(x: UP#Transaction) =
+      if (x.amount > 0 && x.`type` == `type`) Some((x.customerId, x.amount, x.comment))
+      else None
+  }
+  val ValidDebit  = new ValidatingTransactionExtractor(Debit)
+  val ValidCredit = new ValidatingTransactionExtractor(Credit)
+
+  def transactions(from: DateTime, through: DateTime): String ⊕ Seq[UP#Transaction] = databaseMap { implicit session ⇒
     // There's no error handling anywhere here.
 
     // Sbould `successful` turn into a `failed` on exceptions?
-    successful(persistence.transactionsSpanning(from, through).buildColl)
+    persistence.transactionsSpanning(from, through).buildColl.successfulParcel
   }
 
-  def addTransaction(tx: UP#Transaction): String ⊕ UP#Transaction = parcelMap { implicit session ⇒
+  def addTransaction(tx: UP#Transaction): String ⊕ UP#Transaction = databaseMap { implicit session ⇒
     val txId1 = tx match {
-      case persistence.Transaction(_, cid, _, Debit,  amount, comment) if amount > 0 ⇒
+      case ValidDebit(customerId, amount, comment) ⇒
         val txId = for {
-          customer <- persistence findCustomerById cid firstOption
+          customer <- persistence findCustomerById customerId firstOption
         } yield persistence.insertDebit(customer, amount, comment)
 
         txId map successful getOrElse failed("transaction.no_such_customer")
 
-      case persistence.Transaction(_, cid, _, Credit, amount, comment) if amount > 0 ⇒
+      case ValidCredit(customerId, amount, comment) ⇒
         val txId = for {
-          customer <- persistence findCustomerById cid firstOption
+          customer <- persistence findCustomerById customerId firstOption
         } yield persistence.insertCredit(customer, amount, comment)
 
         txId map successful getOrElse failed("transaction.no_such_customer")
 
-      case tx0 if tx0.amount <= 0 ⇒
-        failed("transaction.bad_amount")
+      case tx0 ⇒
+        failed(s"transaction.not_valid: $tx0")
     }
 
     txId1 map (persistence findTransactionById _ firstOption) flatMap {
@@ -144,8 +144,6 @@ class ApplicationFeatures(val persistence: UnifiedPersistence,
   with AuthenticationFeatures
   with CustomerFeatures
   with TransactionFeatures
-
-
 
 /*
 
